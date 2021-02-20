8f14279e98a7b322b9a0957d09d7401ef1be2452
Hugo Gomes <hugo.msn@msn.com>
2021-02-20 20:17:28 -0300

    upgrade packages

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,5 +1,5 @@
-import 'react-native-gesture-handler';
 import React from 'react';
+import { SafeAreaProvider, initialWindowMetrics } from 'react-native-safe-area-context';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { ThemeProvider } from 'styled-components';
 import Routes from './routes';
@@ -7,10 +7,12 @@ import theme from './theme';
 
 export default () => {
   return (
-    <PaperProvider theme={theme}>
-      <ThemeProvider theme={theme}>
-        <Routes />
-      </ThemeProvider>
-    </PaperProvider>
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <PaperProvider theme={theme}>
+        <ThemeProvider theme={theme}>
+          <Routes />
+        </ThemeProvider>
+      </PaperProvider>
+    </SafeAreaProvider>
   );
 };
